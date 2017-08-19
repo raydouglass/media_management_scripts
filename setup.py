7cd16808198653564884b7ac63cc917d04e6c0e6
Ray Douglass <ray@raydouglass.com>
2017-08-19 09:39:56 -0400

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='media_management_scripts',
-      version='0.1.0',
+      version='0.1.1',
       description='Scripts for managing media',
       url='',
       author='Ray Douglass',
