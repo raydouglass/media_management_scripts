3a77fd51e554c1a39df9a84b9042a3d476563d31
Ray Douglass <ray@raydouglass.com>
2017-09-10 12:23:16 -0400

    Adds duration to compare

--- media_management_scripts/commands/metadata_compare.py
+++ media_management_scripts/commands/metadata_compare.py
@@ -1,6 +1,6 @@
 from . import SubCommand
 from .common import *
-from media_management_scripts.support.formatting import sizeof_fmt, duration_to_str
+import os
 
 
 class MetadataCompareCommand(SubCommand):
@@ -16,13 +16,15 @@ class MetadataCompareCommand(SubCommand):
         metadata_parser.add_argument('input', nargs='+')
 
     def subexecute(self, ns):
+        from media_management_scripts.utils import create_metadata_extractor
+        from media_management_scripts.support.formatting import sizeof_fmt, duration_to_str
         input_to_cmd = ns['input']
         interlace = ns['interlace']
         extractor = create_metadata_extractor()
         metadatas = [extractor.extract(i, interlace != 'none') for i in input_to_cmd]
         header = [''] + [os.path.basename(f.file) for f in metadatas]
         num_audio = max([len(m.audio_streams) for m in metadatas])
-        rows = ['Size', 'Bitrate (kb/s)', 'Video Codec', 'Resolution', 'Audio']
+        rows = ['Size', 'Duration', 'Bitrate (kb/s)', 'Video Codec', 'Resolution', 'Audio']
         for i in range(1, num_audio):
             rows.append('')
         rows.append('Subtitles')
@@ -30,6 +32,7 @@ class MetadataCompareCommand(SubCommand):
         for m in metadatas:
             data = []
             data.append(sizeof_fmt(os.path.getsize(m.file)))
+            data.append(duration_to_str(m.estimated_duration) if m.estimated_duration else '')
             data.append('{:.2f}'.format(m.bit_rate / 1024.0))
             video = m.video_streams[0]
             data.append(video.codec)
@@ -42,7 +45,6 @@ class MetadataCompareCommand(SubCommand):
             file_columns.append(data)
         table = list(map(list, zip(*file_columns)))
         self._bulk_print(table, header)
-
 
 
 SubCommand.register(MetadataCompareCommand)
