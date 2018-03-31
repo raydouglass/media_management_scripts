2bf5e293c43e412ab472206662a590d8bdc77001
Ray Douglass <ray@raydouglass.com>
2018-03-31 13:26:16 -0400

    [MMS-1] Fixes the order of combine-subtitles

--- media_management_scripts/commands/combine_subtitles.py
+++ media_management_scripts/commands/combine_subtitles.py
@@ -10,9 +10,10 @@ class CombineSubtitlesCommand(SubCommand):
     def build_argparse(self, subparser):
         combine_video_subtitles_parser = subparser.add_parser('combine-subtitles',
                                                               help='Combine a video files with subtitle file',
-                                                              parents=[parent_parser, input_parser,
-                                                                       convert_parent_parser])
+                                                              parents=[parent_parser, convert_parent_parser])
+        combine_video_subtitles_parser.add_argument('input', help='The input video file')
         combine_video_subtitles_parser.add_argument('input-subtitles', help='The subtitle file')
+        combine_video_subtitles_parser.add_argument('output', help='The destination file')
         combine_video_subtitles_parser.add_argument('--convert',
                                                     help='Whether to convert video streams to H.264 and audio to AAC',
                                                     action='store_const', const=True, default=False)
